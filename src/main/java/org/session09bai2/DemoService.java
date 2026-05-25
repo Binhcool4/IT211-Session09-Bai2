eb63fb89dea9cd4558e7d033cd50f8c0a13da54c
Quoc Binh <binhtrieuhn99@gmail.com>
2026-05-25 15:21:07 +0700

    fix code

--- src/main/java/org/session09bai2/DemoService.java
+++ src/main/java/org/session09bai2/DemoService.java
@@ -5,37 +5,23 @@ import org.slf4j.LoggerFactory;
 
 public class DemoService {
 
-    private static final Logger logger =
-            LoggerFactory.getLogger(DemoService.class);
+    private static final Logger logger = LoggerFactory.getLogger(DemoService.class);
 
-    public void applyDiscount(
-            String userId,
-            String code
-    ) {
+    public void applyDiscount(String userId, String code) {
 
         if (code.equals("VIP")) {
 
             // Thành công
-            logger.info(
-                    "Áp dụng thành công cho user: {}",
-                    userId
-            );
+            logger.info("Áp dụng thành công cho user: {}", userId);
 
         } else if (code.equals("EXPIRED")) {
 
             // Lỗi nghiệp vụ
-            logger.warn(
-                    "Mã giảm giá đã hết hạn: {}",
-                    code
-            );
+            logger.warn("Mã giảm giá đã hết hạn: {}", code);
 
         } else {
-
             // Lỗi hệ thống
-            logger.error(
-                    "Lỗi mất kết nối DB khi check mã: {}",
-                    code
-            );
+            logger.error("Lỗi mất kết nối DB khi check mã: {}", code);
         }
     }
 }
